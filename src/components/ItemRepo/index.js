12552c71bd9ce2e720b0af297e96735ead330ac5
leonardobalves <leonardobuckalves@gmail.com>
2023-04-04 10:16:42 -0300

    projeto finalizado

--- src/components/ItemRepo/index.js
+++ src/components/ItemRepo/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { ItemContainer } from './styles.js';
+import React from 'react'
 
-function ItemRepo() {
-    return (
-        <ItemContainer>
-            <h3>Leo</h3>
-            <p>lelelele</p>
-            <a href='#'>Ver repositório</a><br />
-            <a href='#' className='remover'>Remover</a>
-            <hr />
-        </ItemContainer>
-    )
+import { ItemContainer } from './styles';
+
+function ItemRepo({repo, handleRemoveRepo}) {
+
+  const handleRemove = () => {
+    handleRemoveRepo(repo.id)
+  }
+
+  return (
+    <ItemContainer>
+        <h3>{repo.name}</h3>
+        <p>{repo.full_name}</p>
+        <a href={repo.html_url} rel="noreferrer" target="_blank">Ver repositório</a><br />
+        <a href="#"  rel="noreferrer" onClick={handleRemove} className="remover">Remover</a>
+        <hr />
+    </ItemContainer>
+  )
 }
 
 export default ItemRepo;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,17 +1,42 @@
 import gitHubLogo from '../assets/github-white-logo.png';
 import { Container } from './styles';
 import Input from '../components/Input';
+import Button from '../components/Button';
 import ItemRepo from '../components/ItemRepo';
 import { useState } from 'react';
+import { api } from '../services/api';
+
 
 function App() {
+  const [currentRepo, setCurrentRepo] = useState('');
   const [repos, setRepos] = useState([]);
+
+  const handleSearchRepo = async () => {
+    const {data} = await api.get(`repos/${currentRepo}`)
+    if (data.id) {
+      const isExist = repos.find(repo => repo.id === data.id);
+      if(!isExist){
+        setRepos(prev => [...prev, data]);
+        setCurrentRepo('')
+        return
+      }
+    }
+    alert('Repositório não encontrado')
+  }
+  const handleRemoveRepo = (id) => {
+    setCurrentRepo('');
+    setRepos([]);
+    console.log('Removendo registro', id);
+  }
+
   return (
     <Container>
-      <img src={gitHubLogo} alt='GitHub White Logo PNG' width={72} height={72}/>
-      <Input />
-      <ItemRepo />
-    </Container>);
+      <img src={gitHubLogo} width={72} height={72} alt="github logo"/>
+      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
+      <Button onClick={handleSearchRepo}/>
+      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+    </Container>
+  );
 }
 
 export default App;
